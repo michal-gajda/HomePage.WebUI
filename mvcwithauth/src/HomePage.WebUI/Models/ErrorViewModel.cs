7ae4cbc526671c2b834f0a758f9a60927907cb08
Michal Gajda <michal@gajda.co.uk>
2021-02-16 17:01:44 +0000

    Init

---
+++ mvcwithauth/src/HomePage.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomePage.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
